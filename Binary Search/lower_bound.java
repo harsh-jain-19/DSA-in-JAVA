5d16bef169e5044fa4e402ff503d6fb524efeb81
harsh-jain-19 <harshjain9027@gmail.com>
2025-12-23 20:56:53 +0530

    Adding Lower Bound

---
+++ Binary Search/lower_bound.java
@@ -0,0 +1,101 @@
+// LOWER BOUND : It is the smallest index at which value is greater then or equal to given value.
+// TIME COMPLEXITY = O(log n)
+import java.util.Scanner;
+public class lower_bound
+{
+    public static void quick_sort(int [] arr, int lb, int ub)
+    {
+        int partition;
+        if(lb < ub)
+        {
+            partition = sort(arr,lb,ub);
+            quick_sort(arr,lb,partition-1);
+            quick_sort(arr,partition+1,ub);
+        }
+    }
+    public static int sort(int [] arr, int lb, int ub)
+    {
+        int pivot,i,j,temp;
+        pivot = arr[lb];
+        i = lb;
+        j = ub;
+        while(i < j)
+        {
+            while(arr[i] <= pivot  && i < ub)
+            {
+                i++;
+            }
+            while(arr[j] >= pivot && j > lb)
+            {
+                j--;
+            }
+            if(i < j)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[lb];
+        arr[lb] = arr[j];
+        arr[j] = temp;
+        return(j);
+    }
+    public static int search(int [] arr, int n, int target)
+    {
+        int low,high,mid,ans;
+        low = 0;
+        high = n-1;
+        ans = n;
+        while(low <= high)
+        {
+            mid = (low + high)/2;
+            if(arr[mid] >= target)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an Array");
+        n = sc.nextInt();
+        int [] arr = new int [n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        // AS WE KNOW BINARY SEARCH IS ONLY APPLICABLE ON SORTED ARRAY 
+        int lb,ub;
+        lb = 0;
+        ub = n-1;
+        quick_sort(arr,lb,ub);
+        // BINARY SEARCH
+        int target;
+        System.out.println("Enter the element you are finding.");
+        target = sc.nextInt();
+        int ans = search(arr,n,target);
+        System.out.println("Lower Bound = "+ans);
+        // PRINTING
+        for(int i = 0; i < n; i++)
+        {
+            if(i!=0)
+            {
+                System.out.print(","+arr[i]);
+            }
+            else
+            {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
