fad62f51531e9216306109bdd535849a93d1cd6e
harsh-jain-19 <harshjain9027@gmail.com>
2025-12-25 19:40:15 +0530

    Adding Number Of Occurance

---
+++ Binary Search/number_of_occurance.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+public class number_of_occurance
+{
+    public static void quick_sort(int [] arr, int lb, int ub)
+    {
+        int partition;
+        if(lb < ub)
+        {
+            partition = sort(arr,lb,ub);
+            quick_sort(arr,lb,partition-1);
+            quick_sort(arr,partition+1,ub);
+        }
+    }
+    public static int sort(int [] arr, int lb, int ub)
+    {
+        int pivot,i,j,temp;
+        pivot = arr[lb];
+        i = lb;
+        j = ub;
+        while(i < j)
+        {
+            while(arr[i] <= pivot  && i < ub)
+            {
+                i++;
+            }
+            while(arr[j] >= pivot && j > lb)
+            {
+                j--;
+            }
+            if(i < j)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[lb];
+        arr[lb] = arr[j];
+        arr[j] = temp;
+        return(j);
+    }
+    public static int lower(int [] arr, int n, int target)
+    {
+        int low,high,mid,ans;
+        low = 0;
+        high = n-1;
+        ans = n;
+        while(low <= high)
+        {
+            mid = (low + high)/2;
+            if(arr[mid] >= target)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int upper(int [] arr, int n, int target)
+    {
+        int low,high,mid,ans;
+        low = 0;
+        high = n-1;
+        ans = n;
+        while(low <= high)
+        {
+            mid = (low + high)/2;
+            if(arr[mid] > target)
+            {
+                ans = mid;
+                high = mid -1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return (ans-1);
+    }
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an Array");
+        n = sc.nextInt();
+        int [] arr = new int [n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        // AS WE KNOW BINARY SEARCH IS ONLY APPLICABLE ON SORTED ARRAY 
+        int lb,ub;
+        lb = 0;
+        ub = n-1;
+        quick_sort(arr,lb,ub);
+        // BINARY SEARCH
+        int target,x,y = 0;
+        System.out.println("Enter the element of which you are finding occurance.");
+        target = sc.nextInt();
+        x = lower(arr,n,target);
+        y = upper(arr,n,target);
+        if(x == n || arr[x] != target)
+        {
+            System.out.println(target+" is not in your array.");
+        }
+        else
+        {
+            System.out.println(target+" occured "+((y-x)+1)+" times.");
+        }
+        // PRINTING
+        for(int i = 0; i < n; i++)
+        {
+            if(i!=0)
+            {
+                System.out.print(","+arr[i]);
+            }
+            else
+            {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
