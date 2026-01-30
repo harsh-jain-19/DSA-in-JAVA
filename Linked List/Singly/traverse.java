ea67596705ebedcc9d82b8391cc00be3c7e5cee9
harsh-jain-19 <harshjain9027@gmail.com>
2026-01-30 14:35:13 +0530

    Adding Traverse

---
+++ Linked List/Singly/traverse.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class node
+{
+    int data;
+    node next;
+    node(int d, node n)  //CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = n;
+    }
+    node(int d)  // CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+public class traverse
+{
+    public static void traversal(node head)
+    {
+        node temp = head;
+        while(temp.next != null)
+        {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of your Array.");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        
+        node head = new node(arr[0]);
+        node mover = head;
+        for(int i = 1; i < n; i++)
+        {
+            node temp = new node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        System.out.println(head.data);
+        traversal(head);
+    }
+}
