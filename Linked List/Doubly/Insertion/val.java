2551bc3fd11ad9528bdff4427e41bf9745f658d7
harsh-jain-19 <harshjain9027@gmail.com>
2026-02-09 15:05:28 +0530

    Adding Insert Val In DLL

---
+++ Linked List/Doubly/Insertion/val.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+class node
+{
+    int data;
+    node next;
+    node back;
+    node(int d, node n, node b)  //CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = n;
+        this.back = b;
+    }
+    node(int d)  // CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = null;
+        this.back = null;
+    }
+}
+public class val
+{
+    public static node insert_first(node head, node neww)
+    {
+        node temp = head;
+        temp.back = neww;
+        neww.next = head;
+        head = neww;
+        return head;
+    }
+    public static node insert_last(node head, node neww)
+    {
+        node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = neww;
+        neww.back = temp;
+        return head;
+    }
+    public static node insert_val(node head)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        System.out.println("Enter the value you want to insert.");
+        x = sc.nextInt();
+        node neww = new node(x);
+
+        int val;
+        System.out.println("Enter the value before which you want to insert.");
+        val = sc.nextInt();
+
+        node temp = head;
+        while(temp.next != null)
+        {
+            if(temp.data == val)
+            {
+                break;
+            }
+            temp = temp.next;
+        }
+        if(temp.back == null)
+        {
+            head = insert_first(head,neww);
+            return head;
+        }
+        else if(temp.next == null)
+        {
+            head = insert_last(head,neww);
+            return head;
+        }
+        else
+        {
+            node prev = temp.back;
+            neww.next = temp;
+            neww.back = prev;
+            prev.next = neww;
+            temp.back = neww;
+        }
+        return head;
+    }
+    public static void traversal(node head)
+    {
+        node temp = head;
+        while(temp.next != null)
+        {
+            System.out.print(temp.data+" <=> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of your Array.");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        
+        node head = new node(arr[0]);
+        node prev = head;
+        for(int i = 1; i < n; i++)
+        {
+            node val = new node(arr[i],null,prev);
+            prev.next = val;
+            prev = prev.next;
+        }
+        head = insert_val(head);
+        traversal(head);
+    }
+}
