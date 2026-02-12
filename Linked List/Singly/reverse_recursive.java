3b0d5945a7b018b0ce52fe162d20eb803877174e
harsh-jain-19 <harshjain9027@gmail.com>
2026-02-12 12:40:49 +0530

    Adding Reverse LL

---
+++ Linked List/Singly/reverse_recursive.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+class node
+{
+    int data;
+    node next;
+    node(int d, node n)  //CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = n;
+    }
+    node(int d)  // CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+public class reverse_recursive
+{
+    public static node reverse(node head)
+    {
+        if(head.next == null || head == null)
+        {
+            return head;
+        }
+        node newhead = reverse(head.next);
+        node front = head.next;
+        front.next = head;
+        head.next = null;
+        return newhead;
+    }
+    public static void traversal(node head)
+    {
+        node temp = head;
+        while(temp.next != null)
+        {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of your Array.");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        
+        node head = new node(arr[0]);
+        node temp = head;
+        for(int i = 1; i < n; i++)
+        {
+            node val = new node(arr[i]);
+            temp.next = val;
+            temp = temp.next;
+        }
+        head = reverse(head);
+        traversal(head);
+    }
+}
