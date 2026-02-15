7cc3bd09beb69ed622eb0fe7a8cafdd813b25011
harsh-jain-19 <harshjain9027@gmail.com>
2026-02-15 13:52:23 +0530

    Adding Length Of Loop IN LL

---
+++ Linked List/Singly/lengthOf_loop.java
@@ -0,0 +1,71 @@
+// TORTOISE AND HARE 
+// In this problem return the lenght of a loop/cycle in linked list.
+// Return null if there no loop in linked list.
+import java.util.Scanner;
+class node
+{
+    int data;
+    node next;
+    node(int d, node n)  //CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = n;
+    }
+    node(int d)  // CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+public class lengthOf_loop
+{
+    public static int lengthOfLoop(node head)
+    {
+        int cnt = 0;
+        node slow = head;
+        node fast = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                slow = slow.next;
+                cnt = 1;
+                while(slow != fast)
+                {
+                    slow = slow.next;
+                    cnt++;
+                }
+                return cnt;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of your Array.");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        
+        node head = new node(arr[0]);
+        node temp = head;
+        for(int i = 1; i < n; i++)
+        {
+            node val = new node(arr[i]);
+            temp.next = val;
+            temp = temp.next;
+        }
+
+        temp.next = head.next.next; // This Line Is Used To Form A Loop In Linked List.
+
+        int x = lengthOfLoop(head);
+        System.out.println("Lenght of loop in given LINKED LIST = "+x);
+    }
+}
