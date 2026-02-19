3dbd9967e718e1f52e08b12fd42fe83f5cb17946
harsh-jain-19 <harshjain9027@gmail.com>
2026-02-19 13:03:42 +0530

    Adding Segregate Odd Even Nodes In LL

---
+++ Linked List/Singly/segregate_oddEven.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+class node
+{
+    int data;
+    node next;
+    node(int d, node n)  //CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = n;
+    }
+    node(int d)  // CONSTRUCTOR
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+public class segregate_oddEven
+{
+    public static node odd_even(node head)
+    {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+        node odd = head;
+        node even = head.next;
+        node x = head.next;
+        while(even != null && even.next != null)
+        {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = x;
+        return head;
+    }
+    public static void traversal(node head)
+    {
+        node temp = head;
+        while(temp.next != null)
+        {
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of your Array.");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.nextInt();
+        }
+        
+        node head = new node(arr[0]);
+        node temp = head;
+        for(int i = 1; i < n; i++)
+        {
+            node val = new node(arr[i]);
+            temp.next = val;
+            temp = temp.next;
+        }
+        head = odd_even(head);
+        traversal(head);
+    }
+}
