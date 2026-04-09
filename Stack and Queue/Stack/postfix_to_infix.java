d7f055e6bffc375451e843438839cb98185a7170
harsh-jain-19 <harshjain9027@gmail.com>
2026-04-09 08:55:29 +0530

    Adding Postfix To Infix

---
+++ Stack and Queue/Stack/postfix_to_infix.java
@@ -0,0 +1,52 @@
+// In this Problem we will be given a postfix string .
+// We have to convert it into infix string.
+import java.util.*;
+public class postfix_to_infix
+{
+    public static String infix(String str)
+    {
+        Stack<String> stackk = new Stack<>();
+        int n = str.length();
+        int i = 0;
+        while(i < n)
+        {
+            char ch = str.charAt(i);
+            if(
+                (str.charAt(i) >= 'A'  && str.charAt(i) <= 'Z')
+                || (str.charAt(i) >= 'a'  && str.charAt(i) <= 'z')
+                || (str.charAt(i) >= '0'  && str.charAt(i) <= '9')
+            )
+            {
+                stackk.push(String.valueOf(ch));
+            }
+            else
+            {
+                String t1,t2;
+                t1 = stackk.pop();
+                t2 = stackk.pop();
+                String s = '(' + t2 + str.charAt(i) + t1 + ')';
+                stackk.push(s);
+            }
+            i++;
+        }
+        return stackk.peek();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of your String");
+        int n = sc.nextInt();
+        char [] arr = new char[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the "+i+" element");
+            arr[i] = sc.next().charAt(0);
+        }
+        String str = new String(arr);
+
+        String ans = infix(str);
+
+        System.out.println(str+" (Postfix) = "+ans+" (Infix)");
+
+    }
+}
