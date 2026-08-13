0b4f5c8a75b57f275fe04e6504c4e314140a0258
harsh-jain-19 <harshjain9027@gmail.com>
2026-08-13 12:21:20 +0530

    Adding Longest Common Prefix

---
+++ Strings/longest_common_prefix.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class longest_common_prefix
+{
+    public static String find_longest_common_prefix(String [] strs)
+    {
+        StringBuilder prefix = new StringBuilder();
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        for(int i = 0; i < (Math.min(first.length(),last.length())); i++)
+        {
+            if(first.charAt(i) != last.charAt(i))
+            {
+                return prefix.toString();
+            }
+            prefix.append(first.charAt(i));
+        }
+        return prefix.toString();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of strings you have.");
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] strs = new String[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the value of "+i+"string.");
+            strs[i] = sc.nextLine();
+        }
+
+        System.out.println("Strings you have Entered.");
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(strs[i]+" ,");
+        }
+
+        String prefix = find_longest_common_prefix(strs);
+        if(prefix == "")
+        {
+            System.out.println("\nLongest Common Prefix = null");
+        }
+        else
+        {
+            System.out.println("\nLongest Common Prefix = "+prefix);
+        }
+        
+    }
+}
