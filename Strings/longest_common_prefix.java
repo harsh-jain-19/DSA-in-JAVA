a62ed1e4402ceb64d0fac9ae6f702391816c29a7
harsh-jain-19 <harshjain9027@gmail.com>
2026-08-30 20:57:20 +0530

    Fix formatting issues in longest_common_prefix.java

--- Strings/longest_common_prefix.java
+++ Strings/longest_common_prefix.java
@@ -9,7 +9,7 @@ public class longest_common_prefix
         String first = strs[0];
         String last = strs[strs.length - 1];
 
-        for(int i = 0; i < (Math.min(first.length(),last.length())); i++)
+        
         {
             if(first.charAt(i) != last.charAt(i))
             {
